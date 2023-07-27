e86b6973d6a66ce119aea4eb2867548415fe7bb5
AlessandroFeitosa <100426920+AlessandroFeitosa@users.noreply.github.com>
2023-07-27 20:54:46 -0300

    teste

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const caminhoJson = 'unidades.json';
 var dadosJson = '';
 var arrayCoeficientes = [];
 var casasDecimais = 3;
+var checkComum = false;
 
 const idCategoria = 'selecao-categoria';
 const idTipo = 'selecao-tipo';
@@ -154,13 +155,23 @@ function extraiUnidade(combo) {
         arrayCoeficientes = [];
         // Percorre cada chave (propriedade) do objeto "data"
         let comboboxTipo = objSelectTipo.value;
-        for (let chave in data) {
-            // Verifica se a chave atual tem um atributo "nome"
-            if (data[chave].tipo === comboboxTipo) {
-                nomes.push(data[chave].nick); // Adiciona o nome no array "nomes"
-                arrayCoeficientes.push(data[chave].coeficiente);
-            }
-        }
+        if (checkComum === false){
+            for (let chave in data) {
+                // Verifica se a chave atual tem um atributo "nome"
+                if (data[chave].tipo === comboboxTipo) {
+                    nomes.push(data[chave].nick); // Adiciona o nome no array "nomes"
+                    arrayCoeficientes.push(data[chave].coeficiente);
+                }
+            };
+        } else{
+            for (let chave in data) {
+                // Verifica se a chave atual tem um atributo "nome"
+                if (data[chave].tipo === comboboxTipo && data[chave].frequente === true) {
+                    nomes.push(data[chave].nick); // Adiciona o nome no array "nomes"
+                    arrayCoeficientes.push(data[chave].coeficiente);
+                }
+            };
+        };
         limpaCombobox(combo);
         insereDadosCombobox(removerElementosRepetidos(nomes), combo);
         limpaCombobox(combo + '-2');
@@ -180,6 +191,7 @@ function calcula() {
         objTextUnidade2.value = '';
         return;
     };
+
     let indexSelectUnidade = objSelectUnidade.selectedIndex;
     let indexSelectUnidade2 = objSelectUnidade2.selectedIndex;
     if (indexSelectUnidade === -1) {
@@ -188,16 +200,53 @@ function calcula() {
     if (indexSelectUnidade2 === -1) {
         indexSelectUnidade2 = 0;
     };
-    let coeficienteUnidade = arrayCoeficientes[indexSelectUnidade];
-    let coeficienteUnidade2 = arrayCoeficientes[indexSelectUnidade2];
-    let taxa = coeficienteUnidade / coeficienteUnidade2;
-    objTextUnidade2.value = Math.round(Number(objTextUnidade.value) * taxa * (10 ** casasDecimais)) / (10 ** casasDecimais);
+    if (objSelectCategoria.value === 'Temperatura'){
+        if (indexSelectUnidade === 0 && indexSelectUnidade2 === 1) {
+            objTextUnidade2.value = Math.round((Number(objTextUnidade.value) -273.15) * (10 ** casasDecimais)) / (10 ** casasDecimais);
+        };
+        if (indexSelectUnidade === 1 && indexSelectUnidade2 === 0) {
+            objTextUnidade2.value = Math.round((Number(objTextUnidade.value) +273.15) * (10 ** casasDecimais)) / (10 ** casasDecimais);
+        };
+        if (indexSelectUnidade === 0 && indexSelectUnidade2 === 2) {
+            objTextUnidade2.value = Math.round((Number(objTextUnidade.value) *1.8 -459.67) * (10 ** casasDecimais)) / (10 ** casasDecimais);
+        };
+        if (indexSelectUnidade === 2 && indexSelectUnidade2 === 0) {
+            objTextUnidade2.value = Math.round(((Number(objTextUnidade.value)+459.67)/1.8 ) * (10 ** casasDecimais)) / (10 ** casasDecimais);
+        };
+        if (indexSelectUnidade === 1 && indexSelectUnidade2 === 2) {
+            objTextUnidade2.value = Math.round((Number(objTextUnidade.value) /5*9+32) * (10 ** casasDecimais)) / (10 ** casasDecimais);
+        };
+        if (indexSelectUnidade === 2 && indexSelectUnidade2 === 1) {
+            objTextUnidade2.value = Math.round((Number(objTextUnidade.value) -32) *5/9 * (10 ** casasDecimais)) / (10 ** casasDecimais);
+        };
+        if (indexSelectUnidade === indexSelectUnidade2){
+            objTextUnidade2.value = objTextUnidade.value
+        };
+    } else{
+        let coeficienteUnidade = arrayCoeficientes[indexSelectUnidade];
+        let coeficienteUnidade2 = arrayCoeficientes[indexSelectUnidade2];
+        let taxa = coeficienteUnidade / coeficienteUnidade2;
+        objTextUnidade2.value = Math.round(Number(objTextUnidade.value) * taxa * (10 ** casasDecimais)) / (10 ** casasDecimais);
+    };
+
 }
 
 function decimais() {
     objTextDecimais.innerHTML = objRangeDecimais.value + ' casas decimais';
     casasDecimais = Number(objRangeDecimais.value);
     calcula();
+};
+
+function unidadesComuns(){
+    if (checkComum === true){
+        checkComum = false;
+        extraiUnidade(idUnidade);
+        extraiUnidade(idUnidade2);
+    } else{
+        checkComum = true;
+        extraiUnidade(idUnidade);
+        extraiUnidade(idUnidade2);
+    };
 };
 
 funcaoPrincipal();
